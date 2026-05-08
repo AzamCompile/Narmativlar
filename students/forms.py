9e4e785500fdec13107aca4bcf0680930358d739
AzamComply <sagdullayevazamjon015@gmail.com>
2026-05-08 20:23:14 +0500

    narmativ 88

--- students/forms.py
+++ students/forms.py
@@ -12,3 +12,39 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = "__all__"
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get("phone")
+
+        if not phone.startswith("+998"):
+            raise forms.ValidationError(
+                "Telefon raqam +998 bilan boshlanishi kerak"
+            )
+
+        if len(phone) < 13:
+            raise forms.ValidationError(
+                "Telefon raqam kamida 13 ta belgidan iborat bo‘lishi kerak"
+            )
+
+        return phone
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        if "@" not in email:
+            raise forms.ValidationError(
+                "Email ichida @ bo‘lishi kerak"
+            )
+
+        return email
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get("full_name")
+
+        if len(full_name) < 3:
+            raise forms.ValidationError(
+                "Full name kamida 3 ta belgidan iborat bo‘lishi kerak"
+            )
+
+        return full_name
